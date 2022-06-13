726c24a85de7ed994e9109212fddc75f5e9637c8
Zaykkko <settimewithit@gmail.com>
2022-06-13 00:04:31 -0300

    Added new loaders and plugins

--- babel.config.js
+++ babel.config.js
@@ -5,6 +5,9 @@ module.exports = {
             {
                 useBuiltIns: "usage",
                 corejs: 3,
+                targets: {
+                    browsers: "last 2 versions",
+                },
             },
         ],
         [
